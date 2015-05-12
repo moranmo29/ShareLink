4e26d6254c64546de7930c8375d8a63b7dc4448f
adiGr <adi8554@gmail.com>
2015-05-12 11:50:40 +0300

    appdate to google app engine

---
+++ web/pages/MySavedLinks.py
@@ -0,0 +1,19 @@
+from google.appengine.ext.webapp import template
+
+import webapp2
+class IndexHandler(webapp2.RequestHandler):
+	def get(self):
+		template_params = {}
+#		user = User.checkUser()
+#		if not user:
+#			template_params['loginUrl'] = User.loginUrl()
+#		else:
+#			template_params['logoutUrl'] = User.logoutUrl()
+#			template_params['user'] = user.email
+		
+		html = template.render("web/templates/MySavedLinks.html", template_params)
+		self.response.write(html)
+
+app = webapp2.WSGIApplication([
+	('/MySavedLinks', IndexHandler)
+], debug=True)
---
+++ web/pages/index.py
@@ -0,0 +1,20 @@
+from google.appengine.ext.webapp import template
+
+import webapp2
+class IndexHandler(webapp2.RequestHandler):
+	def get(self):
+		template_params = {}
+#		user = User.checkUser()
+#		if not user:
+#			template_params['loginUrl'] = User.loginUrl()
+#		else:
+#			template_params['logoutUrl'] = User.logoutUrl()
+#			template_params['user'] = user.email
+		
+		html = template.render("web/templates/index.html", template_params)
+		self.response.write(html)
+
+app = webapp2.WSGIApplication([
+	('/', IndexHandler),	
+	('/index', IndexHandler)
+], debug=True)
