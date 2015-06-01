0ce288f8f6e780a1175385f5f5e207bd2929ca41
moranmo29 <moranmo29@gmail.com>
2015-06-01 12:18:46 +0300

    the link from the index page

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -10,6 +10,30 @@ $(".modal-wide").on("show.bs.modal", function() {
   $(this).find(".modal-body").css("max-height", height);
 });
 
+function sendEmail(){
+	var mailfrom = $('#mailfromid').val();
+    var to = $('#to').val();
+	var url_link = $('#url_link').val();
+	var description = $('#description').val();
+	$.ajax({
+		url:'/sendmail',
+		type:'GET',
+		dataType:'json',
+        data:{mailfrom:mailfrom, to:to,url_link:url_link,description:description},
+		success:function(data, status, xhr) {
+            alert("success!!");            
+			location.reload();
+
+		},
+		error:function(xhr, status, error) {
+            alert("the send email failed!!");
+			console.error(xhr, status, error);
+		//	window.location.href = "/index";	
+		}
+	});
+
+}
+
 function submitLogin() {
     var email = $('#login_email').val();
     var password = $('#login_password').val();
@@ -24,7 +48,7 @@ function submitLogin() {
 
 		},
 		error:function(xhr, status, error) {
-            alert("the login faild!!");
+            alert("the login failed!!");
 			console.error(xhr, status, error);
 			window.location.href = "/index";	
 		}
@@ -44,9 +68,15 @@ function submitRegister() {
 		   window.location.href = "/mysavedlinks";	
 		},
 		error:function(xhr, status, error) {
-            alert("sign up faild");
+            alert("sign up failed");
 			//alert(xhr.responseText);
 			console.error(xhr, status, error);
 		}
 	});
+}
+
+
+function setsendEmail() {
+		$('#url_link').val($('#urlpageindex').val());
+		
 }
