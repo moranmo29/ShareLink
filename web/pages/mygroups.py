5735d715e1faa35b60a9065a553a69c138ff66aa
adiGr <adi8554@gmail.com>
2015-06-15 12:23:03 +0300

    take care of groups

--- web/pages/mygroups.py
+++ web/pages/mygroups.py
@@ -19,50 +19,17 @@ class IndexHandler(webapp2.RequestHandler):
 		template_params['useremail'] = user.email
 		grouplist= Group.getAllGroups(user)
 		groups= []
-#		if grouplist:
-#			for group in grouplist:
-#				group_name= group.group_name
-#				admin= group.admin
-#				groupandadmin=[group_name, admin]
-#				groups.append(groupandadmin)
-#			template_params['groups']= groups
-
-#		group = Group.get_by_id(int(group_id))
-#		if group.admin != user.key and user.key not in group.members:
-#			template_params['no_access'] = True
-#		else:
-#			template_params['group_name'] = group.group_name
-#			template_params['group_admin'] = group.admin.get().email
-#			template_params['group_id'] = group_id
+		if grouplist:
+			for group in grouplist:
+				group_name= group['group_name']
+				groupid=group['id']
+				one=[group_name,groupid]
+				groups.append(one)
+			template_params['groups']= groups
 		
 		html = template.render("web/templates/mygroups.html", template_params)
 		self.response.write(html)
-
-class addGroup(webapp2.RequestHandler):
-	def get(self):
-		template_params={}
-		group_name=self.request.get('group_name')
-		members= json.loads(self.request.get('members'))
-		user = None
-		if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
-			user = User.checkToken(self.request.cookies.get('our_token'))
-		if not user:
-			html = template.render("web/templates/index.html", {})
-			self.response.write(html)
-			return
-		template_params['useremail'] = user.email
-		group= Group()
-		group.group_name=group_name
-		group.admin= user.key
-		for newM in members:
-			usermem= User.checkIfUesr(newM)
-			group.members.append(usermem.key)
-		group.put()
-		self.response.set_cookie('our_token', str(user.key.id()))
-		self.response.write(json.dumps({'status':'OK'}))
-		return
-
+		
 app = webapp2.WSGIApplication([
-	('/mygroups', IndexHandler),
-	('/addGroup',addGroup)
+	('/mygroups', IndexHandler)
 ], debug=True)
