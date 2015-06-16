e860aec687514a85aefd69dfe4d2a776d761b434
adiGr <adi8554@gmail.com>
2015-06-16 12:43:43 +0300

    Revert "change fix bugs in useremail"
    This reverts commit 956bab6642c7cff6b3ad2ca8859e6e9e57a8c542.

--- web/pages/contactuspage.py
+++ web/pages/contactuspage.py
@@ -20,6 +20,7 @@ class IndexHandler(webapp2.RequestHandler):
 		if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
 			user = User.checkToken(self.request.cookies.get('our_token'))
 		if user:
+			template_params['useremail'] = user.email
 			#newlinks
 			linkslist=Link.getAllLinksPerUser(user)	
 			newurls = []
@@ -32,8 +33,7 @@ class IndexHandler(webapp2.RequestHandler):
 					if fromlink is not None:
 						urlandlink =[url,des,fromlink]
 						newurls.append(urlandlink)
-			template_params['newurls'] = newurls
-			template_params['useremail'] = user.email	
+				template_params['newurls'] = newurls
 			#newlinks
 		html = template.render("web/templates/contactuspage.html", template_params)
 		self.response.write(html)
