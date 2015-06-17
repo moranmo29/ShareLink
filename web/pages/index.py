087773a0e8ec3f665091d8f4514d76cbae3e0f54
liran <liran.94.h@gmail.com>
2015-06-17 10:30:30 +0300

    change new urls in index py

--- web/pages/index.py
+++ web/pages/index.py
@@ -4,6 +4,7 @@ import json
 import webapp2
 import string
 from models.user import User
+from models.link import Link
 
 class IndexHandler(webapp2.RequestHandler):
 	def get(self):
@@ -18,6 +19,20 @@ class IndexHandler(webapp2.RequestHandler):
 		if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
 			user = User.checkToken(self.request.cookies.get('our_token'))
 		if user:
+			#newlinks
+			linkslist=Link.getAllLinksPerUser(user)	
+			newurls = []
+			template_params = {}
+			if linkslist:
+				for link in linkslist:
+					url = link.url_link
+					des = link.description
+					fromlink=link.from_link
+					if fromlink is not None:
+						urlandlink =[url,des,fromlink]
+						newurls.append(urlandlink)
+				template_params['newurls'] = newurls
+			#newlinks
 			template_params['useremail'] = user.email			
 		html = template.render("web/templates/index.html", template_params)
 		self.response.write(html)
