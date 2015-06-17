64ae8b4468652c61643be6c48fdc1e0d9bde973c
liran <liran.94.h@gmail.com>
2015-06-17 10:28:52 +0300

    change newurls in contact us py

--- web/pages/contactuspage.py
+++ web/pages/contactuspage.py
@@ -18,6 +18,20 @@ class IndexHandler(webapp2.RequestHandler):
 		if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
 			user = User.checkToken(self.request.cookies.get('our_token'))
 		if user:
+			#newlinks
+			linkslist=Link.getAllLinksPerUser(user)	
+			newurls = []
+			template_params = {}
+			if linkslist:
+				for link in linkslist:
+					url = link.url_link
+					des = link.description
+					fromlink=link.from_link
+					if fromlink is not None:
+						urlandlink =[url,des,fromlink]
+						newurls.append(urlandlink)
+				template_params['newurls'] = newurls
+			#newlinks
 			template_params['useremail'] = user.email
 		html = template.render("web/templates/contactuspage.html", template_params)
 		self.response.write(html)
