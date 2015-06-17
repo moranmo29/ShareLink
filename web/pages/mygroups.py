26b04976ee67ba704fa70f57c36983fdb0d74a39
liran <liran.94.h@gmail.com>
2015-06-17 10:31:23 +0300

    changes newurls in mygroups

--- web/pages/mygroups.py
+++ web/pages/mygroups.py
@@ -16,6 +16,20 @@ class IndexHandler(webapp2.RequestHandler):
 			html = template.render("web/templates/index.html", {})
 			self.response.write(html)
 			return
+		#newlinks
+		linkslist=Link.getAllLinksPerUser(user)	
+		newurls = []
+		template_params = {}
+		if linkslist:
+			for link in linkslist:
+				url = link.url_link
+				des = link.description
+				fromlink=link.from_link
+				if fromlink is not None:
+					urlandlink =[url,des,fromlink]
+					newurls.append(urlandlink)
+			template_params['newurls'] = newurls
+		#newlinks
 		template_params['useremail'] = user.email
 		grouplist= Group.getAllGroups(user)
 		groups= []
