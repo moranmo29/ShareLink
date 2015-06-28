9014ca2b6ed776117dadd2116deab8fa077ca81b
moranmo29 <moranmo29@gmail.com>
2015-06-28 12:10:55 +0300

    add function - find link in table

--- web/static/js/tabeljs.js
+++ web/static/js/tabeljs.js
@@ -121,3 +121,18 @@ function ValidURL(str) {
     return true;
   }
 }
+
+function searchLink(){
+		$("#search").keyup(function (){
+		var value = this.value.toLowerCase().trim();
+		$("table tr").each(function (index) {
+			if (!index) return;
+			$(this).find("td").each(function () {
+				var id = $(this).text().toLowerCase().trim();
+				var not_found = (id.indexOf(value) == -1);
+				$(this).closest('tr').toggle(!not_found);
+				return not_found;
+			});
+    });
+	});
+}
